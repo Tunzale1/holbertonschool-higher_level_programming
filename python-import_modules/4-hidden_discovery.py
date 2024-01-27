a48e228f8937c69b557e1a2b84a1ef39c5f1ff0c
Tunzale1 <tunzaleimanova03@mail.ru>
2024-01-27 13:17:24 -0800

    ya haqq

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4, sys
+import hidden_4
+import sys
 if __name__ == "__main__":
     show = dir(hidden_4)
     for i in show:
-        if i[0] != "_" or i[1] != "_":
+        if i[0] != "_":
             print("{}".format(i))
