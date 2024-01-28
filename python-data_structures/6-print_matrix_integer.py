2355b5919d4ad630c2640c77f005c98075033ddf
Tunzale1 <tunzaleimanova03@mail.ru>
2024-01-28 07:30:35 -0800

    matrixxx

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in range(len(row)):
-            print("{:d}".format(row[num]), end=" " if num != len(row) - 1 else "")
+            if num != len(row) - 1:
+                print("{:d}".format(row[num]), end=" ")
+            else:
+                print("{:d}".format(row[num]), end="")
         print()
