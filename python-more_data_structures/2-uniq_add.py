44d904fe4cc2bf4200dcf3057cfa02898e1ed2d7
Tunzale1 <tunzaleimanova03@mail.ru>
2024-01-30 02:45:38 -0800

    uniquee numbers not even numbers

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    seen = set()
     total = 0
     for i in my_list:
-        if i % 2 != 0:
+        if i not in seen:
             total += int(i)
+            seen.add(i)
     return(total)
