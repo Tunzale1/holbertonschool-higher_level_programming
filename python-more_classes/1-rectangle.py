715b332e5b4e700c235168500f650f6cd34ef483
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-05 02:43:44 -0800

    hhh

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,19 +9,6 @@ class Rectangle:
         self.__height = height
 
     @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """private att"""
-        self.__width = value
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-
-    @property
     def height(self):
         return self.__height
 
@@ -33,3 +20,16 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """private att"""
+        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
