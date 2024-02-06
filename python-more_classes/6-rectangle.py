cb3b7718b56c41e687ba89fdd2f220ddbdd1c9bd
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-06 02:06:33 -0800

    doc problem solved

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -3,8 +3,9 @@
 
 
 class Rectangle:
-    number_of_instances = 0
     ''' rectangle'''
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
