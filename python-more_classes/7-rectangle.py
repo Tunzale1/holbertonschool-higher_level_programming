0470856af208affc1131c3322af6773326122661
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-06 01:57:46 -0800

    baa bu defe kecmelidii

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -3,9 +3,10 @@
 
 
 class Rectangle:
+    ''' rectangle'''
     number_of_instances = 0
     print_symbol = "#"
-    ''' rectangle'''
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
