87e487a7436829c3f96572424febe8a28a3e26f9
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-08 01:01:46 -0800

    isinstance()

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''saaaamm'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''if the object is an instance of, or if the object is an instance'''
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
