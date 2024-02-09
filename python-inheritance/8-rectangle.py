513ade2c3d5cc6df5f398319ef5e60b8c6201945
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-09 02:36:52 -0800

    push

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''adeyyo lalaleyyo'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    '''no need to ask'''
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
