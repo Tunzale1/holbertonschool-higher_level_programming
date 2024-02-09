e0ec548757faeb9e4f6f11f09175eb6ba0dcad1a
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-09 06:17:36 -0800

    everything is okay!!!!!

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""i am vengeance"""
+''' i'm vengeance '''
 
 
-Rectangle = __import__("9-rectangle").Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """i am the night"""
     def __init__(self, size):
-        """i am  batman"""
+        """I am the night"""
         self.integer_validator('size', size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+        """ i am batman"""
         return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
+
