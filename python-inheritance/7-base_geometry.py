fa65ba788ace76c8c823009aa0f2d101818acb0d
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-09 02:43:43 -0800

    fkfg

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,7 +5,7 @@
 class BaseGeometry:
     '''smoooothh operatoorr'''
 
-    def are(self):
+    def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
