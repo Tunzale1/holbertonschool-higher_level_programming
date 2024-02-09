9ca00604a0cd585d97014d47a247413a139cf76d
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-09 06:20:50 -0800

    i am vengeance!!!!

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -6,16 +6,15 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """I am the night"""
     def __init__(self, size):
-        """I am the night"""
+        """ i am batman"""
         self.integer_validator('size', size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """ i am batman"""
         return self.__size ** 2
 
     def __str__(self):
         return f"[Square] {self.__size}/{self.__size}"
-
