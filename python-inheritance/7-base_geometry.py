1273bb5d8f15e6835194163f16d320a54bdda285
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-09 02:01:54 -0800

    7. Integer validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''he bele bele isler'''
+
+
+class BaseGeometry:
+    '''smoooothh operatoorr'''
+
+    def are(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value < 0 or value == 0:
+            raise ValueError(f"{name} must be greater than 0")
+
