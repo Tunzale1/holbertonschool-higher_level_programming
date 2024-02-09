94da7e89305f5ef519a9a31640a6c642b9e5fd69
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-09 02:42:22 -0800

    j

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(BaseGeometry):
     '''no need to ask'''
 
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         BaseGeometry.integer_validator(self, "width", width)
         BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
