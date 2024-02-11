bdab5b713dc34660fe157dda085451e0fcb4aa7f
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-11 09:21:09 -0800

    is it okay?

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Student to JSON with filter'''
+
+
+class Student:
+    '''student class'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        student_dict = {}
+        for attr in (attrs or dir(self)):
+            if not attr.startswith('_'):  # Exclude private attributes
+                value = getattr(self, attr)
+                if not callable(value):  # Exclude methods
+                    student_dict[attr] = value
+
+        return student_dict
