09279b1932efd5de935932c4f76aa6b5dca0e083
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-11 04:35:50 -0800

    pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''inpout'''
+
+
 def read_file(filename=""):
-    with open (filename, encoding = "utf-8") as f:
+    '''read'''
+    with open(filename, encoding="utf-8") as f:
         content = f.read()
         print(content)
