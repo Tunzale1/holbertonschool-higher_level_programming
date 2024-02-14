2fcd167741e4d7f4df0223346e69f4262be80ce0
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-14 07:07:48 -0800

    square clas

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''square'''
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''class'''
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(id, x, y, size, size)
+
+    def __str__(self):
+        return "[Square] ({}) {}\
+                /{} - {}".format(self.id, self.x, self.y, self.size)
+
+    @property
+    def size(self):
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
