56855852e626765bb63cbf5b69ce3aed4471651b
Tunzale1 <tunzaleimanova03@mail.ru>
2024-02-15 02:14:27 -0800

    checkerr!

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,7 +38,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
-        if json_string is None or "":
+        if json_string is None or " ":
             return "[]"
         else:
             return json.loads(json_string)
