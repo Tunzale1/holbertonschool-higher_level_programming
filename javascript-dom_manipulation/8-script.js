3a7724c61d2007b184eafdceb0cf1291e7526314
Tunzale Imanova <114104944+Tunzale1@users.noreply.github.com>
2024-05-21 13:04:33 +0400

    say hello!!

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,16 +1,10 @@
-// Listen for the "DOMContentLoaded" event to ensure the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
-    // Use the Fetch API to get data from the provided URL
     fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
       .then(response => {
-        // Convert the response to JSON
         return response.json();
       })
       .then(data => {
-        // Get the element with id "hello"
         let helloElement = document.getElementById('hello');
-        
-        // Set the content of the div to the "hello" value from fetched data
         helloElement.textContent = data.hello;
       })
       .catch(error => {
