2a06fbe72cec7bf4a0974a92f119fd4b2a998c96
Tunzale Imanova <114104944+Tunzale1@users.noreply.github.com>
2024-05-21 13:07:38 +0400

    list of elements

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,16 +1,8 @@
-// Step 1: Get the element with id "add_item"
 let addItemElement = document.getElementById('add_item');
 
-// Step 2: Add an event listener for the click event on this element
 addItemElement.addEventListener('click', function() {
-
-    // Step 3: Create a new list item
     let newListItem = document.createElement('li');
     newListItem.textContent = 'Item';
-
-    // Step 4: Get the list with class "my_list"
     let myList = document.querySelector('.my_list');
-
-    // Step 5: Append the new list item to this list
     myList.appendChild(newListItem);
 });
