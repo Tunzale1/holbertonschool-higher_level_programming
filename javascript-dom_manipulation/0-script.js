3ae7389980e0e4da98c8beee47b04edb06c4d58d
Tunzale Imanova <114104944+Tunzale1@users.noreply.github.com>
2024-05-21 13:11:05 +0400

    updates text color

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,5 +1,3 @@
-// Using document.querySelector to select the header element
 let headerElement = document.querySelector('header');
 
-// Update the text color of the header element to red (#FF0000)
 headerElement.style.color = '#FF0000';
