35f90106d1883a590f649ccce2d29113d9440242
Tunzale Imanova <114104944+Tunzale1@users.noreply.github.com>
2024-05-21 13:06:15 +0400

    fetches the character name

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,14 +1,10 @@
-// Step 1: Use the Fetch API to get data from the provided URL
 fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
   .then(response => {
-    // Step 2: Convert the response to JSON
     return response.json();
   })
   .then(data => {
-    // Step 3: Get the element with id "character"
     let characterElement = document.getElementById('character');
     
-    // Step 4: Update the text content of the "character" div with the fetched name
     characterElement.textContent = data.name;
   })
   .catch(error => {
