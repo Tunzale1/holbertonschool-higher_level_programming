36dd465ed471152247146010607ca516a87ca9c5
Tunzale Imanova <114104944+Tunzale1@users.noreply.github.com>
2024-05-21 13:08:09 +0400

    hot fix

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,11 +1,8 @@
-// Get the element with id "toggle_header"
 let toggleHeaderElement = document.getElementById('toggle_header');
 
-// Add an event listener for the click event
 toggleHeaderElement.addEventListener('click', function() {
     let headerElement = document.querySelector('header');
 
-    // Check the current class and toggle between "red" and "green"
     if (headerElement.classList.contains('green')) {
         headerElement.classList.remove('green');
         headerElement.classList.add('red');
