3e75a976eb3ee69b933814895d98343569559c78
Tunzale Imanova <114104944+Tunzale1@users.noreply.github.com>
2024-05-21 13:10:00 +0400

    adds the class

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,8 +1,5 @@
-// Select the element with id "red_header"
 let redHeaderElement = document.getElementById('red_header');
 
-// Add an event listener for the click event
 redHeaderElement.addEventListener('click', function() {
-    // Select the header element and add the class "red"
     document.querySelector('header').classList.add('red');
 });
